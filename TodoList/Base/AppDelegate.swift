d1cd2782e3389f04fd6a983626af05beaed6a561
Deisy Melo <deisy.melo@walmart.com>
2023-09-08 21:39:30 -0500

    Firebase config

--- TodoList/Base/AppDelegate.swift
+++ TodoList/Base/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window = window
         self.appCoordinator = AppCoordinator(window: window)
         appCoordinator?.start()
+        
+        FirebaseApp.configure()
         
         return true
     }
